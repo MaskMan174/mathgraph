7253e816485e6cb0899fc2b19815d8f324f2b3c6
MaskMan174 <84721213+MaskMan174@users.noreply.github.com>
2022-10-24 23:17:58 +0300

    Hallelujah

--- monte_carlo.py
+++ monte_carlo.py
@@ -21,16 +21,6 @@ def gr_and_leas(f, sol, a, b):
     return arr[0], arr[len(arr) - 1]
 
 
-'''def s_monte_carlo(func, a, b, h, h0, iterations):
-    from random import random
-    count = 0
-    for i in range(iterations):
-        x, y = random() * (b - a) + a, random() * (h - h0) + h0
-        if (func(x) < y and (y < 0)) or (func(x) > y and (y > 0)):
-            count += 1
-    return count'''
-
-
 def stripped_s_monte_carlo(func, a, b, h, h0):
     count = 0
     func = pk.loads(func)
